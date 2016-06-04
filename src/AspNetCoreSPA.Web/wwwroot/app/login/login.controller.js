29d8d957dc6da1dfbb2ea0cb83759319f4ca2967
ToanNM4 <nmtoan07@gmail.com>
2016-06-04 20:17:53 +0700

    Remove unused variable

--- src/AspNetCoreSPA.Web/wwwroot/app/login/login.controller.js
+++ src/AspNetCoreSPA.Web/wwwroot/app/login/login.controller.js
@@ -7,20 +7,17 @@
 
     function LoginController($scope, $http, $state, auth0Service) {
         var vm = this;
-
+        
         vm.loginInfo = {
             UserName: "test01",
             Password: "Qwer!@#12345"
         };
 
         vm.login = function () {
-            vm.dataLoading = true;
             auth0Service.login(vm.loginInfo, function (response) {
                 if (response == "OK") {
                     auth0Service.authenticate();
                     $state.go('main');
-                } else {
-                    vm.dataLoading = false;
                 }
             });
         }
